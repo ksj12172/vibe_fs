4c1c9a0f248b0d5e43ebb22101f8720b466a5e0b
ksj12172 <sojeong94@naver.com>
2025-06-16 00:37:32 +0900

    문구 수정

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import './globals.css';
 
 export const metadata = {
   title: '재무제표 시각화',
-  description: '한국 상장기업의 재무제표를 쉽게 조회하고 시각화해보세요',
+  description: '한국 상장기업의 재무제표를 조회하고 분석해봐요.',
 };
 
 export default function RootLayout({ children }) {

361c5d89485f0ad41e94d1cfe8e7c4ab3ca67dff
ksj12172 <sojeong94@naver.com>
2025-07-21 02:04:55 +0900

    feat: OHLCAnalyzer에 추가적인 분석 로직 및 시각화 개선

--- components/OHLCAnalyzer.tsx
+++ components/OHLCAnalyzer.tsx
@@ -13,6 +13,7 @@ const getAnalysis = ({
   const totalRange = high - low;
   const isDoji = Math.abs(open - close) <= totalRange * 0.01;
   const isBullish = !isDoji && close > open;
+  const isBearish = !isDoji && close < open;
 
   const upperWick = high - Math.max(open, close);
   const lowerWick = Math.min(open, close) - low;
@@ -20,17 +21,23 @@ const getAnalysis = ({
   const bodySize = Math.abs(open - close);
   const bodyRatio = bodySize / totalRange;
 
-  const isLongLowerWick =
-    lowerWick >= bodySize * 1.5 && upperWick <= bodySize * 0.5;
-  const tailThreshold = totalRange * 0.01;
+  const tailThreshold = totalRange * 0.1;
+  const hasSignificantTailThreshold = totalRange * 0.3;
+  const longTailThreshold = totalRange * 0.5;
+
   const hasNoUpperWick = upperWick <= tailThreshold;
 
   /**
-   * 장대 몸통
+   * 장대 몸통, 추세 주도성이 있음
    * 양봉 > 꾸준한 매수세
    * 음봉 > 꾸준한 매도세
    */
   const isLongBody = bodySize >= totalRange * 0.6;
+  /**
+   * 짧은 몸통
+   * 방향성이 뚜렷하지 않음 (중립)
+   */
+  const isShortBody = bodySize <= totalRange * 0.4;
 
   /**
    * 장대 양봉 + 매수 주도형 케이스
@@ -49,6 +56,48 @@ const getAnalysis = ({
     lowerWick > tailThreshold
   ) {
     return "장대 양봉 + 윗꼬리 & 아랫꼬리 모두 있는 전형적인 매수 우위 캔들입니다.\n 일부 매도세가 유입되어 아랫꼬리가 형성되었습니다. \n 눌림 이후 매수세가 강하게 유입되며 반등했습니다. \n 고점 근접 후 일부 이익 실현이 이루어졌으나, 매수 주도로 상승 마감했습니다.\n 다음 날 추가 상승을 기대할 수 있는 긍정적 흐름입니다.\n";
+  }
+
+  /**
+   * 짧은 양봉 + 아랫꼬리 긴 케이스
+   */
+  if (
+    isBullish &&
+    isShortBody &&
+    lowerWick > longTailThreshold &&
+    upperWick > tailThreshold
+  ) {
+    return "긴 아랫꼬리를 가진 짧은 양봉입니다.\n눌림 후 매수세가 유입되었으나, 매수세가 확실한 우위는 아닙니다.\n 방향성이 뚜렷하지 않습니다.\n 보합 내지 반등 가능성이 있으나, 다음 날 거래량 증가와 함께 고가 돌파 여부가 중요한 관건입니다.";
+  }
+
+  /**
+   * 짧은 양봉 && 윗꼬리/아랫꼬리 모두 일정 비율 이상 존재
+   */
+  if (
+    isBullish &&
+    isShortBody &&
+    lowerWick > hasSignificantTailThreshold &&
+    upperWick > hasSignificantTailThreshold
+  ) {
+    return "상하 꼬리가 긴 중립 양봉입니다.\n 매수세가 존재하지만 뚜렷한 우위는 아닙니다. \n 시장 혼조 or 불확실한 조정 구간에서 자주 나타나는 패턴입니다.\n";
+  }
+
+  /**
+   * 장대 음봉
+   */
+  if (isBearish && isLongBody && hasNoUpperWick && lowerWick > tailThreshold) {
+    return "하락 지속형 약세 음봉입니다.\n 윗꼬리 없이 시가가 고점, 종가가 저점 근처라면, 매도세가 하루 종일 지속됐다는 의미입니다.\n아랫꼬리가 있으나 매수세가 강하지 않아 저점 부근 마감했습니다. \n다음 날 추가 하락 가능성도 열려 있습니다.\n 거래량이 많았다면 신뢰도가 높습니다. 적었다면 단기 조정일 수 있습니다.\n";
+  }
+
+  /**
+   * 장대 음봉, 아랫 꼬리
+   */
+  if (
+    isBearish &&
+    upperWick < tailThreshold &&
+    lowerWick > hasSignificantTailThreshold
+  ) {
+    return "매도 주도 음봉입니다.\n 장 초반 약간 상승했지만 곧바로 매도가 우세했습니다. \n 종가가 저점보단 높아, 하단 지지나 반등 시도가 있었다고 볼 수 있습니다.\n 하지만 전체적으로 하락 주도세가 뚜렷합니다.\n 다만, 눌림 반발 가능성이 약간 있습니다. 하단 지지 구간인지 여부가 중요합니다.";
   }
 
   let analysis = "";
@@ -71,7 +120,7 @@ const getAnalysis = ({
       } else {
         analysis += "아랫꼬리가 더 깁니다. \n";
         if (isBullish) {
-          if (isLongLowerWick) {
+          if (lowerWick > longTailThreshold) {
             analysis +=
               "장중에 매도세가 있었지만, 이후 강한 매수세가 들어와 강한 상승폭을 보였습니다. \n 저점이라면, 저가 매수세가 유입하여 반등 가능성, 지지선 확인 후 상승 전환 시도로 볼 수 있습니다. \n";
           } else {
@@ -184,6 +233,21 @@ export default function OHLCAnalyzer({
 
   const orderedPriceList = priceList.sort((a, b) => b.value - a.value);
 
+  const HIGHT_LIST = [
+    {
+      label: "윗꼬리",
+      value: ((upperWick / totalHeight) * 100).toFixed(2) + "%",
+    },
+    {
+      label: "몸통",
+      value: ((bodySize / totalHeight) * 100).toFixed(2) + "%",
+    },
+    {
+      label: "아랫꼬리",
+      value: ((lowerWick / totalHeight) * 100).toFixed(2) + "%",
+    },
+  ];
+
   return (
     <div style={{ marginTop: "20px" }}>
       <h3 style={{ margin: "10px 0" }}>OHLC 분석</h3>
@@ -201,21 +265,21 @@ export default function OHLCAnalyzer({
             <div
               style={{
                 width: "2px",
-                height: ((upperWick / totalHeight) * 100).toFixed(2) + "%",
+                height: HIGHT_LIST[0].value,
                 backgroundColor: color,
               }}
             ></div>
             <div
               style={{
                 width: "20px",
-                height: ((bodySize / totalHeight) * 100).toFixed(2) + "%",
+                height: HIGHT_LIST[1].value,
                 backgroundColor: color,
               }}
             ></div>
             <div
               style={{
                 width: "2px",
-                height: ((lowerWick / totalHeight) * 100).toFixed(2) + "%",
+                height: HIGHT_LIST[2].value,
                 backgroundColor: color,
               }}
             ></div>
@@ -234,6 +298,16 @@ export default function OHLCAnalyzer({
               <span className="text-gray-500">{price.name}: </span>
               <span className="ml-2 font-medium text-red-600">
                 {price.formattedValue}
+              </span>
+            </div>
+          ))}
+        </div>
+        <div>
+          {HIGHT_LIST.map((height) => (
+            <div key={height.label}>
+              <span className="text-gray-500">{height.label}: </span>
+              <span className="ml-2 font-medium text-red-600">
+                {height.value}
               </span>
             </div>
           ))}
